92e632f3fd8c8d2cbe4255f99fe9a89979092516
muhammadSaifurrehman985 <159415417+muhammadSaifurrehman985@users.noreply.github.com>
2024-03-07 20:43:42 +0500

    Add files via upload

---
+++ addition/app1.js
@@ -0,0 +1,11 @@
+"use strict";
+// operators in typescript
+//operator number 1 ADDITION
+// the sign of this operator is similar to our math book sign (+)
+//example 1 
+console.log(12 + 18); // its output should be 30.
+//example 2
+let apple = (12);
+let mangoes = (18);
+let both = (apple + mangoes);
+console.log(both); // in this case output shoulde be the same as example 1.
---
+++ addition/app1.ts
@@ -0,0 +1,16 @@
+// operators in typescript
+//operator number 1 ADDITION
+// the sign of this operator is similar to our math book sign (+)
+
+//example 1 
+console.log(12 + 18);// its output should be 30.
+
+
+//example 2
+
+let apple: number = (12);
+let mangoes : number = (18);
+
+let both = (apple+mangoes);
+
+console.log(both);// in this case output shoulde be the same as example 1.
