84f3901556f0ec401b4db244f249eab8bc02c429
muhammadSaifurrehman985 <159415417+muhammadSaifurrehman985@users.noreply.github.com>
2024-03-07 20:48:48 +0500

    Add files via upload

---
+++ Exponentiation/app1.js
@@ -0,0 +1,11 @@
+"use strict";
+// operators in typescript
+//operator number 5 EXPONENTIATION(POWER).
+// the sign of this operator is NOT similar to our math book sign. ITS SIGN IS(**)
+//example 1 
+console.log(4 ** 2); // its output will be 16.
+//example 2
+let whiteApple = (2);
+let redApple = (5);
+let both = (redApple ** whiteApple);
+console.log(both); // in this case output will be 25.
---
+++ Exponentiation/app1.ts
@@ -0,0 +1,16 @@
+// operators in typescript
+//operator number 5 EXPONENTIATION(POWER).
+// the sign of this operator is NOT similar to our math book sign. ITS SIGN IS(**)
+
+//example 1 
+console.log(4 ** 2);// its output will be 16.
+
+
+//example 2
+
+let whiteApple: number = (2);
+let redApple : number = (5);
+
+let both = (redApple**whiteApple);
+
+console.log(both);// in this case output will be 25.
