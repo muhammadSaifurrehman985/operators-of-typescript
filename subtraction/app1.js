60b57279665727565896c863ae1d529bebde4d95
muhammadSaifurrehman985 <159415417+muhammadSaifurrehman985@users.noreply.github.com>
2024-03-07 20:52:33 +0500

    Add files via upload

---
+++ subtraction/app1.js
@@ -0,0 +1,11 @@
+"use strict";
+// operators in typescript
+//operator number 2 SUBTRACTION.
+// the sign of this operator is similar to our math book sign (-)
+//example 1 
+console.log(12 - 18); // its output will be -6.
+//example 2
+let dogs = (12);
+let cats = (18);
+let both = (cats - dogs);
+console.log(both); // in this case output will be 6.
---
+++ subtraction/app1.ts
@@ -0,0 +1,16 @@
+// operators in typescript
+//operator number 2 SUBTRACTION.
+// the sign of this operator is similar to our math book sign (-)
+
+//example 1 
+console.log(12 - 18);// its output will be -6.
+
+
+//example 2
+
+let dogs: number = (12);
+let cats : number = (18);
+
+let both = (cats-dogs);
+
+console.log(both);// in this case output will be 6.
