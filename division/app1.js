2734c0b9fc124f929c53cf3a12bd82d18c678c8b
muhammadSaifurrehman985 <159415417+muhammadSaifurrehman985@users.noreply.github.com>
2024-03-07 20:48:17 +0500

    Add files via upload

---
+++ division/app1.js
@@ -0,0 +1,11 @@
+"use strict";
+// operators in typescript
+//operator number 4 DIVISION
+// the sign of this operator is NOT similar to our math book sign.ITS SIGN IS (/)
+//example 1 
+console.log(12 / 120); // its output will be 0.1 .
+//example 2
+let apple = (12);
+let mangoes = (120);
+let both = (apple / mangoes);
+console.log(both); // in this case output shoulde be the same as example 1.
